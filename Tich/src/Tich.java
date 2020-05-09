87fb93aaf70d8065758b38d6a8fad9c72345f235
TienNguyen-08 <cnnguyenvantien@gmail.com>
2020-05-09 18:27:40 +0700

    tich

---
+++ Tich/src/Tich.java
@@ -0,0 +1,15 @@
+
+public class Tich {
+
+	public int Tich(int a,int b) {
+		// TODO Auto-generated constructor stub
+		return a*b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tich tich = new Tich();
+		tich.Tich(4, 4);
+	}
+
+}
